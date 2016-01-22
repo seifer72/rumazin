73f4090ea5fe8a72741a5136dba2a36ef2e17e91
Rzhavin Timofey <seifer72@yandex.ru>
2016-01-22 13:12:05 +0300

    Init progect

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ruvoin_session'
+Rails.application.config.session_store :cookie_store, key: '_rumazin_session'
